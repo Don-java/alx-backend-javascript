a1d4caaf21d5761e3b8830ee0351da413b3f92d9
don-java <donijilad@gmail.com>
2023-06-26 22:47:39 -0700

    ES6 Promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  let firstName;
+  let lastName;
+  let body;
+
+  return Promise.all([uploadPhoto(), createUser()]).then((data) => {
+    body = data[0].body;
+    firstName = data[1].firstName;
+    lastName = data[1].lastName;
+    console.log(`${body} ${firstName} ${lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
